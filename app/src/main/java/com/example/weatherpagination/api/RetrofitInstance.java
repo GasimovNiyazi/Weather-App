1f2c2319aed70720d8a00585d2bf7576538864f2
Niyazi19 <niyazi.qasimov.99@gmail.com>
2019-04-01 00:17:13 +0400

    Add Interceptor

--- app/src/main/java/com/example/weatherpagination/api/RetrofitInstance.java
+++ app/src/main/java/com/example/weatherpagination/api/RetrofitInstance.java
@@ -1,5 +1,7 @@
 package com.example.weatherpagination.api;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,15 +9,23 @@ public class RetrofitInstance {
 
     private final static String BASE_URL="https://api.darksky.net/";
 
+     private  static HttpLoggingInterceptor mLoggingInterceptor=new HttpLoggingInterceptor();
+
 
     private  static Retrofit sRetrofit;
 
     public static Retrofit getRetrofitInstance(){
 
+        mLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient=new OkHttpClient.Builder()
+                .addInterceptor(mLoggingInterceptor)
+                .build();
+
         if (sRetrofit == null) {
             sRetrofit=new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
  return sRetrofit;
